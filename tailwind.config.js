b5191e7357e1e6fddbf5433f6c64715d19dd0518
boraicoz <94445388+boraicoz@users.noreply.github.com>
2026-02-20 16:58:25 +0300

    Added files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      }
+    }
+  },
+  plugins: []
+}
